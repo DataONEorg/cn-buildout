5a1f9eb8067fe7b7c378b303fcddbebfd4eafe5d
Robert Waltz <rwaltz@utk.edu>
2010-05-14 16:02:25 +0000

    finally comitting changes from syncronization procedure

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -1,10 +1,3 @@
-
-
-rem  mysql -u root -ppassword < nbii_mysql_exec_setup_3b.sql
-
-rem java -Xms512m -Xmx1024m -jar  ..\Harvest_dataone.jar nbii nbii nodebug nbii
-
-rem java -Xms512m -Xmx1024m -jar ..\Solr_feed_R21.jar nbii nbii nodebug mercury3_harvests_nbii nbii.log schema_xpath2.xml
-
-
-
+mysql -u mercuryuser -pmercury3user < datanet_setup.sql
+java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
+java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
