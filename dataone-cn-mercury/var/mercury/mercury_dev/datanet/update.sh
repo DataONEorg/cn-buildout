28e890f8a52f74d0b53f6f4d96ea6aa6769f9a06
Robert Waltz <rwaltz@utk.edu>
2010-05-19 04:12:30 +0000

    Task (#438): edit the Makefile

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -1,3 +1,3 @@
-mysql -u root -pdataone < /var/mercury/mercury_dev/datanet/datanet_setup.sql
-java -Xms512m -Xmx1024m -jar  /var/mercury/mercury_dev/datanet/Harvest_dataone.jar datanet ornldaac nodebug daac
-java -Xms512m -Xmx1024m -jar /var/mercury/mercury_dev/datanet/Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
+mysql -u root -pdataone < datanet_setup.sql
+java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
+java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
