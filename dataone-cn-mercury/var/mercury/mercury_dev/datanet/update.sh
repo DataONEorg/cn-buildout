71e722fa4ed575e687ed4c644e428a4a8a3b4f71
Robert Waltz <rwaltz@utk.edu>
2010-05-19 05:26:03 +0000

    taking out the changing dir and moving into postinst

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ## expand the war file
-CURR_DIR=`pwd`
 /etc/init.d/mysql stop
 /etc/init.d/mysql start
 
@@ -10,7 +9,4 @@ cd /var/mercury/mercury_dev/datanet/
 mysql -u root -pdataone < datanet_setup.sql
 java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
 java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
-echo cd to $CURR_DIR
-cd $CURR_DIR
-
 
