d30e4d2805b2e04df39793ec3c2954a7defee93a
Robert Waltz <rwaltz@utk.edu>
2010-05-20 22:13:19 +0000

    Update for testing with merged metadata

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -7,6 +7,8 @@
 echo "Configuring mysql, importing data to solr and indexing solr"
 cd /var/mercury/mercury_dev/datanet/
 mysql -u root -pdataone < datanet_setup.sql
-java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
-java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
+##java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
+##java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
+java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet daacmerged nodebug daac
+java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet daacmerged nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
 
