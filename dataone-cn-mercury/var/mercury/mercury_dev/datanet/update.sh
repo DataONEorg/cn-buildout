0790c5ec99281ee266e5ad37d348f50ee2f4a48b
Robert Waltz <rwaltz@utk.edu>
2010-05-22 03:25:28 +0000

    (#277) Run Integration Testing package  -- need these exposed for debuggin
    and some for 
    (#291): Run Integration Testing package

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -10,6 +10,6 @@ cd /var/mercury/mercury_dev/datanet/
 mysql -u root -pdataone < datanet_setup.sql
 java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
 java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
-##java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet daacmerged nodebug daacmerged
-##java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet daacmerged nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
+java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet daacmerged nodebug daac
+java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet daacmerged nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
 
