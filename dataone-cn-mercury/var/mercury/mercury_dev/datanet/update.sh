bf312a70fee301ecbfddda79b77cf16159f6184c
Robert Waltz <rwaltz@utk.edu>
2010-06-06 22:57:35 +0000

    (#631) Rebuild cn-dev with revised applications

--- dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
+++ dataone-cn-mercury/var/mercury/mercury_dev/datanet/update.sh
@@ -4,12 +4,15 @@
 /etc/init.d/mysql start
 
 ## make knb directory and extract cn.war into it.
-echo "Configuring mysql, importing data to solr and indexing solr"
+echo "Configuring mysql,packaging files, importing data to solr, and indexing solr"
 #mysqladmin -u root password 'dataone'
 cd /var/mercury/mercury_dev/datanet/
 mysql -u root -pdataone < datanet_setup.sql
 #java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet ornldaac nodebug daac
 #java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet ornldaac nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
-java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet daacmerged nodebug daac
-java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet daacmerged nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
+
+java -Xms512m -Xmx1024m -jar dataone-cn-packager-0.4.0.jar
+
+java -Xms512m -Xmx1024m -jar  Harvest_dataone.jar datanet daacpackage nodebug daac
+java -Xms512m -Xmx1024m -jar Solr_feed_R23.jar datanet daacpackage nodebug mercury3_harvests_datanet datanet.log datanet_schema_xpath2.xml
 
