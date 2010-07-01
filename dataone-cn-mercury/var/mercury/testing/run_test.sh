5af195a6aa31a7c1acade06bc29ebd25803f8bac
Rob Nahf <rnahf@epscor.unm.edu>
2010-07-01 20:51:18 +0000

    redirecting output to output folder

--- dataone-cn-mercury/var/mercury/testing/run_test.sh
+++ dataone-cn-mercury/var/mercury/testing/run_test.sh
@@ -1,4 +1,4 @@
 # the first script produces TAP output (ok / not_ok)
 # the second converts it to JUnit / ANT output format
 # that should be suitable for Hudson.
-perl test_xpath_v_sampleData.pl | perl tap-to-junit-xml.pl --puretap > junit-output-pt.xml 
+perl ./test_xpath_v_sampleData.pl | perl ./tap-to-junit-xml.pl --puretap > t_outputs/xpathFind_v_sampleData.xml
