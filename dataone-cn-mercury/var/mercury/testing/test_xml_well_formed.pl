4ca878d439d185e4086cba269ba04ef2012584d9
Rob Nahf <rnahf@epscor.unm.edu>
2010-07-02 15:27:01 +0000

    added comments

--- dataone-cn-mercury/var/mercury/testing/test_xml_well_formed.pl
+++ dataone-cn-mercury/var/mercury/testing/test_xml_well_formed.pl
@@ -1,3 +1,15 @@
+
+# ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+#    This is a testing program that produces TAP output
+#  (ok / not_ok) for each field to index.
+#
+#    This program tests whether or not the mercury xml
+#  files themselves are well formed.  This can be a good
+#  diagnostic for when changes are made to the files 
+#  over time.
+#  
+# ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
 use lib "./lib";
 use Test::More;
 use XML::Parser;
