1f2dcf2edbf92ae29794db883b322c9a690fad09
Robert Waltz <rwaltz@utk.edu>
2011-02-15 19:01:29 +0000

    Stories #1341

--- dataone-cn-mercury/usr/share/mercury/debian/googlekey.js
+++ dataone-cn-mercury/usr/share/mercury/debian/googlekey.js
@@ -16,9 +16,23 @@ else if(curlocation.indexOf("http://devportal.nbii.gov")!=-1)
 {
 	gkey = "ABQIAAAAMunL2PF_-ibJlwJr-tMA8RQLuipRujvM7qWlxjrJnIe2s1oi8xTSFwtaQrDYa8ye7QCQV79PJBQcQg";
 }
+else if(curlocation.indexOf("http://cn-ucsb-1.dataone.org") < 10)
+{
+  gkey = "ABQIAAAAuYWC81lXHUzQz1QJga3l8hR3ud-BIj7E0PLxOb5nKvE6iYKfchRZSstsTbW5r5h83q7b8WThhfo86A";
+}
+else if(curlocation.indexOf("http://cn-unm-1.dataone.org") < 10)
+{
+  gkey = "ABQIAAAAuYWC81lXHUzQz1QJga3l8hSS04c4NbGTDqdmpwFWLo7aT0wv0xQUqNfrRjXMkW4z87Wn38Zpy4KIVg";
+}
+else if(curlocation.indexOf("http://cn-orc-1.dataone.org") < 10)
+{
+  gkey = "ABQIAAAAuYWC81lXHUzQz1QJga3l8hSSDrPAcYjZlkF6Tkq3u_Fyu3mCCxSQYYUlOrQcODgjecV1RvXjMXhfAQ";
+}
 else
-{ 
- gkey = "ABQIAAAAMunL2PF_-ibJlwJr-tMA8RTkFTU-bOiazHtZ0nNnypynHqSO4hRZnPrnn0_VUhIEMt-cWpvnGi2-Kg";
+{
+ //gkey = "ABQIAAAAMunL2PF_-ibJlwJr-tMA8RTkFTU-bOiazHtZ0nNnypynHqSO4hRZnPrnn0_VUhIEMt-cWpvnGi2-Kg";
+  //default key is for "http://cn.dataone.org" 
+  gkey = "ABQIAAAAuYWC81lXHUzQz1QJga3l8hQeSVgniQmrIwOXFpLeToaRJdxWixSppZ3XA2lcOViCzbkUYKuLuhXaWg";
 }
 tag = "<scr" + "ipt src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key="+gkey+"\" type=\"text/javascript\"></scr" + "ipt>";
 
