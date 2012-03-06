85019e322aac82ef6e418df698a055bd3ae3af9c
Robert Waltz <rwaltz@utk.edu>
2012-03-06 20:46:55 +0000

    superseded by dataone-mercury package

--- dataone-cn-mercury/var/mercury/testing/run_test.sh
+++
@@ -1,4 +0,0 @@
-# the first script produces TAP output (ok / not_ok)
-# the second converts it to JUnit / ANT output format
-# that should be suitable for Hudson.
-perl ./test_xpath_v_sampleData.pl | perl ./tap-to-junit-xml.pl --puretap > t_outputs/xpathFind_v_sampleData.xml
